9c612ea8d2d4319d16f434d8081f1bbf6982b624
marx <89142977+xyldxal@users.noreply.github.com>
2025-08-08 13:59:45 +0700

    remove unnecessary files

--- scripts/verifyProof.js
+++
@@ -1,7 +0,0 @@
-const { execSync } = require('child_process');
-const path = require('path');
-
-const buildDir = path.join(__dirname, '../build');
-
-console.log('Verifying proof...');
-execSync(`snarkjs groth16 verify ${path.join(buildDir, 'verification_key.json')} ${path.join(buildDir, 'public.json')} ${path.join(buildDir, 'proof.json')}`, { stdio: 'inherit' });
